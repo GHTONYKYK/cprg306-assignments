2492858ac4be4e0b553b0c3fc9f64d807f5bdd2e
Tony Kim <158311131+GHTONYKYK@users.noreply.github.com>
2024-09-28 01:02:21 -0600

    Week 3 assignment submission

--- app/page.js
+++ app/page.js
@@ -8,6 +8,19 @@ export default function HomePage() {
       <Link href="/week-2" legacyBehavior>
         <a>Week 2 Assignment</a>
       </Link>
+      <br></br>
+      <Link href="/week-3" legacyBehavior>
+        <a>Week 3 Assignment</a>
+      </Link>
+      <br></br>
+      <Link href="/week-4" legacyBehavior>
+        <a>Week 4 Assignment</a>
+      </Link>
+      <br></br>
+      <Link href="/week-5" legacyBehavior>
+        <a>Week 5 Assignment</a>
+      </Link>
+      <br></br>
     </div>
   );
 }
